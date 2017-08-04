201b40e5fad474cb64e415c29dae06c0658f5e90
Magnus Gether Sørensen <magesoe@gmail.com>
2017-08-04 08:22:10 +0200

    New version 0.3.5

--- src/XrmMockup365/Properties/AssemblyInfo.cs
+++ src/XrmMockup365/Properties/AssemblyInfo.cs
@@ -4,16 +4,16 @@ using System.Reflection;
 [assembly: AssemblyTitleAttribute("XrmMockup365")]
 [assembly: AssemblyProductAttribute("XrmMockup")]
 [assembly: AssemblyDescriptionAttribute("A simulation engine that can mock a specific MS CRM instance. Useful for testing and debugging business logic.")]
-[assembly: AssemblyVersionAttribute("0.3.4")]
-[assembly: AssemblyFileVersionAttribute("0.3.4")]
+[assembly: AssemblyVersionAttribute("0.3.5")]
+[assembly: AssemblyFileVersionAttribute("0.3.5")]
 [assembly: AssemblyCompanyAttribute("Delegate A/S")]
 namespace System {
     internal static class AssemblyVersionInformation {
         internal const System.String AssemblyTitle = "XrmMockup365";
         internal const System.String AssemblyProduct = "XrmMockup";
         internal const System.String AssemblyDescription = "A simulation engine that can mock a specific MS CRM instance. Useful for testing and debugging business logic.";
-        internal const System.String AssemblyVersion = "0.3.4";
-        internal const System.String AssemblyFileVersion = "0.3.4";
+        internal const System.String AssemblyVersion = "0.3.5";
+        internal const System.String AssemblyFileVersion = "0.3.5";
         internal const System.String AssemblyCompany = "Delegate A/S";
     }
 }
