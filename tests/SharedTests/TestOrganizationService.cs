4a9b8d46973b5fec210e9ec360c39b8a0b2abcd0
Henrik Hannemose <hha@delegate.dk>
2017-09-12 16:08:51 +0200

    Added failing unit test: It is possible to call CreateOrganizationService for a user that does not exist

--- tests/SharedTests/TestOrganizationService.cs
+++ tests/SharedTests/TestOrganizationService.cs
@@ -18,10 +18,11 @@ namespace SharedTests
             crm.ResetEnvironment();
             var newAdminUser = crm.AdminUser.Id;
 
+            //Should it be possible to create an OrganizationService for a non-existent user?
             var origService = crm.CreateOrganizationService(origAdminUser);
             using (var context = new Xrm(origService))
             {
-                //Currently throws an exception. Should return null
+                //Currently throws an exception
                 var acc = context.AccountSet.FirstOrDefault(); 
                 Assert.IsNull(acc);
             }
