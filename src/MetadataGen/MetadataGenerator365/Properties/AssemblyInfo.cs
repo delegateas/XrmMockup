9712efca0f71189fd808c0e48614d0f8dd62fa9d
Magnus Gether Sørensen <magesoe@gmail.com>
2017-12-08 09:14:20 +0100

    Published nuget 0.4.4

--- src/MetadataGen/MetadataGenerator365/Properties/AssemblyInfo.cs
+++ src/MetadataGen/MetadataGenerator365/Properties/AssemblyInfo.cs
@@ -4,16 +4,16 @@ using System.Reflection;
 [assembly: AssemblyTitleAttribute("MetadataGenerator365")]
 [assembly: AssemblyProductAttribute("XrmMockup")]
 [assembly: AssemblyDescriptionAttribute("A simulation engine that can mock a specific MS CRM instance. Useful for testing and debugging business logic.")]
-[assembly: AssemblyVersionAttribute("0.4.3")]
-[assembly: AssemblyFileVersionAttribute("0.4.3")]
+[assembly: AssemblyVersionAttribute("0.4.4")]
+[assembly: AssemblyFileVersionAttribute("0.4.4")]
 [assembly: AssemblyCompanyAttribute("Delegate A/S")]
 namespace System {
     internal static class AssemblyVersionInformation {
         internal const System.String AssemblyTitle = "MetadataGenerator365";
         internal const System.String AssemblyProduct = "XrmMockup";
         internal const System.String AssemblyDescription = "A simulation engine that can mock a specific MS CRM instance. Useful for testing and debugging business logic.";
-        internal const System.String AssemblyVersion = "0.4.3";
-        internal const System.String AssemblyFileVersion = "0.4.3";
+        internal const System.String AssemblyVersion = "0.4.4";
+        internal const System.String AssemblyFileVersion = "0.4.4";
         internal const System.String AssemblyCompany = "Delegate A/S";
     }
 }
