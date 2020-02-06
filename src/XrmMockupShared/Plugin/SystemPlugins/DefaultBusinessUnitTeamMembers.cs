70052858b4f644f77da9382c86b879c6ed1f54b8
TST <TST@DESKTOP-Q698959>
2020-02-06 10:56:04 +0100

    Polished tests and logic for handling deffault teams and its members #80

---
+++ src/XrmMockupShared/Plugin/SystemPlugins/DefaultBusinessUnitTeamMembers.cs
@@ -0,0 +1,128 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using System;
+using System.ServiceModel;
+using Microsoft.Crm.Sdk.Messages;
+
+namespace DG.Tools.XrmMockup.SystemPlugins
+{
+    internal class DefaultBusinessUnitTeamMembers : MockupPlugin
+    {
+        IOrganizationService orgAdminService;
+        IOrganizationService orgService;
+
+        internal DefaultBusinessUnitTeamMembers() : base(typeof(DefaultBusinessUnitTeams))
+        {
+            RegisterPluginStep(LogicalNames.SystemUser,
+                PluginEventOperation.Create,
+                PluginExecutionStage.PostOperation,
+                AddMember);
+
+            RegisterPluginStep(LogicalNames.SystemUser,
+                PluginEventOperation.Delete,
+                PluginExecutionStage.PreOperation,
+                RemoveMember);
+
+            RegisterPluginStep(LogicalNames.SystemUser,
+                PluginEventOperation.Update,
+                PluginExecutionStage.PreOperation,
+                UpdatePreOperation);
+
+            RegisterPluginStep(LogicalNames.SystemUser,
+                PluginEventOperation.Update,
+                PluginExecutionStage.PostOperation,
+                UpdatePostOperation);
+        }
+
+        private void UpdatePostOperation(LocalPluginContext localContext)
+        {
+            HandleServices(localContext);
+
+            if (IsBusinessUnitIdChanged(localContext))
+            {
+                AddMember(localContext);
+            }
+        }
+
+        private void UpdatePreOperation(LocalPluginContext localContext)
+        {
+            HandleServices(localContext);
+
+            if (IsBusinessUnitIdChanged(localContext))
+            {
+                RemoveMember(localContext);
+            }
+        }
+
+        private bool IsBusinessUnitIdChanged(LocalPluginContext localContext)
+        {
+            return true;
+        }
+
+
+        private void RemoveMember(LocalPluginContext localContext)
+        {
+            HandleServices(localContext);
+
+            var retrievedSystemUser = orgService.Retrieve("systemuser", localContext.PluginExecutionContext.PrimaryEntityId, new ColumnSet("businessunitid"));
+            
+            var team = GetBusinessUnitDefaultTeam(retrievedSystemUser);
+
+            var request = new RemoveMembersTeamRequest
+            {
+                TeamId = team.Id,
+                MemberIds = new[] {retrievedSystemUser.Id}
+            };
+
+            orgService.Execute(request);
+        }
+
+        private void AddMember(LocalPluginContext localContext)
+        {
+            HandleServices(localContext);
+
+            var retrievedSystemUser = orgService.Retrieve("systemuser", localContext.PluginExecutionContext.PrimaryEntityId, new ColumnSet("businessunitid"));
+            
+            var team = GetBusinessUnitDefaultTeam(retrievedSystemUser);
+
+            var request = new AddMembersTeamRequest
+            {
+                TeamId = team.Id,
+                MemberIds = new[] {retrievedSystemUser.Id}
+            };
+
+            orgService.Execute(request);
+        }
+
+
+        private void HandleServices(LocalPluginContext localContext)
+        {
+            if (localContext == null)
+            {
+                throw new ArgumentNullException("localContext");
+            }
+
+            orgAdminService = localContext.OrganizationAdminService;
+            orgService = localContext.OrganizationService;
+        }
+
+        private Entity GetBusinessUnitDefaultTeam(Entity retrievedSystemUser)
+        {
+            EntityReference businessUnitReference = (EntityReference)retrievedSystemUser.Attributes["businessunitid"];
+
+            var teamQuery = new QueryExpression("team");
+            teamQuery.ColumnSet = new ColumnSet("isdefault", "businessunitid");
+            teamQuery.Criteria.AddCondition("isdefault", ConditionOperator.Equal, true);
+            teamQuery.Criteria.AddCondition("businessunitid", ConditionOperator.Equal, businessUnitReference.Id);
+
+            var retrievedTeams = orgService.RetrieveMultiple(teamQuery);
+
+            if (retrievedTeams.Entities.Count > 1)
+            {
+                throw new FaultException("There cannot be more than one default business unit team!");
+            }
+
+            return orgService.RetrieveMultiple(teamQuery).Entities[0];
+        }
+    }
+}
---
+++ tests/SharedTests/TestDefaultBusinessUnitTeamsMembers.cs
@@ -0,0 +1,121 @@
+﻿using DG.Tools.XrmMockup;
+using DG.XrmFramework.BusinessDomain.ServiceContext;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+
+namespace DG.XrmMockupTest
+{
+    [TestClass]
+    public class TestDefaultBusinessUnitTeamMembers : UnitTestBase
+    {
+        BusinessUnit businessUnit1;
+        BusinessUnit businessUnit2;
+        BusinessUnit businessUnit3;
+        BusinessUnit businessUnit4;
+        private SystemUser user1;
+        private SystemUser user2;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            EntityReference businessUnitId = crm.RootBusinessUnit;
+
+            businessUnit1 = new BusinessUnit { ParentBusinessUnitId = businessUnitId, Name = "Business Unit 1" };
+            businessUnit1.Id = orgAdminService.Create(businessUnit1);
+            
+            businessUnit2 = new BusinessUnit { ParentBusinessUnitId = businessUnitId, Name = "Business Unit 2" };
+            businessUnit2.Id = orgAdminService.Create(businessUnit2);
+            user1 = crm.CreateUser(orgAdminService, businessUnit2.ToEntityReference(), SecurityRoles.SystemCustomizer) as SystemUser;
+
+            businessUnit3 = new BusinessUnit { ParentBusinessUnitId = businessUnitId, Name = "Business Unit 3" };
+            businessUnit3.Id = orgAdminService.Create(businessUnit3);
+            businessUnit4 = new BusinessUnit { ParentBusinessUnitId = businessUnitId, Name = "Business Unit 4" };
+            businessUnit4.Id = orgAdminService.Create(businessUnit4);
+            user2 = crm.CreateUser(orgAdminService, businessUnit3.ToEntityReference(), SecurityRoles.SystemCustomizer) as SystemUser;
+        }
+
+        [TestMethod]
+        public void AddMembersToBusinessUnit()
+        {
+            var team = RetrieveBusinessUnitDefaultTeamAndCheckAttributes(businessUnit1);
+
+            SystemUser user11 = crm.CreateUser(orgAdminService, businessUnit1.ToEntityReference(), SecurityRoles.SystemCustomizer) as SystemUser;
+
+            using (var context = new Xrm(orgAdminService))
+            {
+                var fetchedTeam = context.TeamMembershipSet.Where(x => x.TeamId == team.Id).ToList();
+                Assert.AreEqual(1, fetchedTeam.Count);
+            }
+        }
+
+        [TestMethod]
+        public void RemoveMemberFromBusinessUnit()
+        {
+            var team = RetrieveBusinessUnitDefaultTeamAndCheckAttributes(businessUnit2);
+
+            orgAdminService.Delete("systemuser", user1.Id);
+
+            using (var context = new Xrm(orgAdminService))
+            {
+                var fetchedTeam = context.TeamMembershipSet.Where(x => x.TeamId == team.Id).ToList();
+                Assert.AreEqual(0, fetchedTeam.Count);
+            }
+        }
+
+        [TestMethod]
+        public void MoveMemberFromBUOneToBUTwo()
+        {
+            var retrievedUser = orgAdminService.Retrieve("systemuser", user2.Id, new ColumnSet("businessunitid"));
+
+            retrievedUser.Attributes["businessunitid"] = businessUnit4.ToEntityReference();
+
+            orgAdminService.Update(retrievedUser);
+
+            var team3 = RetrieveBusinessUnitDefaultTeamAndCheckAttributes(businessUnit3);
+            var team4 = RetrieveBusinessUnitDefaultTeamAndCheckAttributes(businessUnit4);
+
+            using (var context = new Xrm(orgAdminService))
+            {
+                var fetchedTeam = context.TeamMembershipSet.Where(x => x.TeamId == team3.Id).ToList();
+                Assert.AreEqual(0, fetchedTeam.Count);
+            }
+
+            using (var context = new Xrm(orgAdminService))
+            {
+                var fetchedTeam = context.TeamMembershipSet.Where(x => x.TeamId == team4.Id).ToList();
+                Assert.AreEqual(1, fetchedTeam.Count);
+            }
+        }
+
+        private Team RetrieveBusinessUnitDefaultTeamAndCheckAttributes(BusinessUnit businessUnit)
+        {
+            using (var context = new Xrm(orgAdminUIService))
+            {
+                var fetchedTeam = context.TeamSet
+                    .Where(x => x.BusinessUnitId.Id == businessUnit.Id)
+                    .Where(x => x.Name == businessUnit.Name)
+                    .FirstOrDefault();
+
+                Assert.IsNotNull(fetchedTeam);
+                CheckTeamAttributes(fetchedTeam, businessUnit);
+
+                return fetchedTeam;
+            }
+        }
+
+        private void CheckTeamAttributes(Team fetchedTeam, BusinessUnit businessUnit)
+        {
+            businessUnit = (BusinessUnit)orgAdminService.Retrieve(LogicalNames.BusinessUnit, businessUnit.Id, new ColumnSet("name", "createdby"));
+
+            Assert.AreEqual(businessUnit.Name, fetchedTeam.Name);
+            Assert.AreEqual(Team_TeamType.Owner, fetchedTeam.TeamType);
+            Assert.AreEqual(true, fetchedTeam.IsDefault);
+            Assert.AreEqual("Default team for the parent business unit. The name and membership for default team are inherited from their parent business unit.",
+                fetchedTeam.Description);
+            Assert.AreEqual(businessUnit.CreatedBy.Id, fetchedTeam.AdministratorId.Id);
+            Assert.AreEqual(businessUnit.Id, fetchedTeam.BusinessUnitId.Id);
+        }
+    }
+}
