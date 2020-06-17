9c6a35382fe19923c180e77f3af6a398c7c51fc8
Thomas Kjærgaard Malowanczyk <tkm@delegate.dk>
2020-06-17 09:56:29 +0200

    Release new version 1.2.5

--- src/MetadataGen/MetadataGenerator13/Properties/AssemblyInfo.cs
+++ src/MetadataGen/MetadataGenerator13/Properties/AssemblyInfo.cs
@@ -4,16 +4,16 @@ using System.Reflection;
 [assembly: AssemblyTitleAttribute("MetadataGenerator13")]
 [assembly: AssemblyProductAttribute("XrmMockup")]
 [assembly: AssemblyDescriptionAttribute("A simulation engine that can mock a specific MS CRM instance. Useful for testing and debugging business logic.")]
-[assembly: AssemblyVersionAttribute("1.2.4")]
-[assembly: AssemblyFileVersionAttribute("1.2.4")]
+[assembly: AssemblyVersionAttribute("1.2.5")]
+[assembly: AssemblyFileVersionAttribute("1.2.5")]
 [assembly: AssemblyCompanyAttribute("Delegate A/S")]
 namespace System {
     internal static class AssemblyVersionInformation {
         internal const System.String AssemblyTitle = "MetadataGenerator13";
         internal const System.String AssemblyProduct = "XrmMockup";
         internal const System.String AssemblyDescription = "A simulation engine that can mock a specific MS CRM instance. Useful for testing and debugging business logic.";
-        internal const System.String AssemblyVersion = "1.2.4";
-        internal const System.String AssemblyFileVersion = "1.2.4";
+        internal const System.String AssemblyVersion = "1.2.5";
+        internal const System.String AssemblyFileVersion = "1.2.5";
         internal const System.String AssemblyCompany = "Delegate A/S";
     }
 }
