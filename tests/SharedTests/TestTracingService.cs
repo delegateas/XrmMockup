add84620d2afca863298c7145ffab74ce5b447a6
Matt Trinder <matt.trinder@tisski.com>
2021-01-06 11:40:37 +0000

    add test

---
+++ tests/SharedTests/TestTracingService.cs
@@ -0,0 +1,24 @@
+﻿using DG.XrmFramework.BusinessDomain.ServiceContext;
+using DG.XrmContext;
+using Xunit;
+using Microsoft.Xrm.Sdk;
+using DG.Tools.XrmMockup;
+
+namespace DG.XrmMockupTest
+{
+    public class TestTracingService : UnitTestBase
+    {
+        public TestTracingService(XrmMockupFixture fixture) : base(fixture) { }
+
+        [Fact]
+        public void TracingServiceShouldAcceptXMLFormatStrings()
+        {
+
+            ITracingService trace = new TracingService();
+            trace.Trace("<xml>formatted</xml>");
+            
+
+            
+        }
+    }
+}
