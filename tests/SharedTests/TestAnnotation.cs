d8c70ff46f5c8af3526a96feaf7ef1152f494734
Matt Trinder <matt.trinder@tisski.com>
2021-01-15 15:30:00 +0000

    failing test

---
+++ tests/SharedTests/TestAnnotation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using DG.XrmFramework.BusinessDomain.ServiceContext;
+using Xunit;
+
+namespace DG.XrmMockupTest
+{
+    public class TestAnnotation : UnitTestBase
+    {
+        public TestAnnotation(XrmMockupFixture fixture) : base(fixture) { }
+
+        [Fact]
+        public void TestAnnotationsWithAttachmentsHaveIsDocumentSet()
+        {
+            var note = new Entity("annotation");
+            note["documentbody"] = "base64string";
+            var noteId = orgAdminService.Create(note);
+
+            var checkNote = orgAdminService.Retrieve("annotation", noteId, new ColumnSet(true));
+            Assert.True(checkNote.GetAttributeValue<bool>("isdocument"));
+
+        }
+    }
+}
