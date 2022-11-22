e04fcce24e8ee96ce9311fef760cc43c6c717bdb
Morten Holt <mho@delegate.dk>
2022-11-22 14:46:21 +0100

    Add testing for the .NET6 builds

---
+++ tests/TestPluginAssembly365/WorkflowMock.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+/// <summary>
+/// Implementations of the different Workflow related attributes
+/// These are part of the SDK package, which we do not have access to in .NET6 builds
+/// </summary>
+
+#if NET6_0_OR_GREATER
+namespace Microsoft.Xrm.Sdk.Workflow
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public abstract class ParameterAttribute : Attribute
+    {
+        public string Name { get; private set ;}
+
+        protected ParameterAttribute(string name)
+        {
+            Name = name;
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Property)]
+    public sealed class InputAttribute : ParameterAttribute
+    {
+        public InputAttribute(string name)
+            : base(name)
+        {
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Property)]
+    public sealed class OutputAttribute : ParameterAttribute
+    {
+        public OutputAttribute(string name)
+            : base(name)
+        {
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Property)]
+    public sealed class ReferenceTargetAttribute : Attribute
+    {
+        public string EntityName { get; set; }
+
+        public ReferenceTargetAttribute(string entityName)
+        {
+            EntityName = entityName;
+        }
+    }
+}
+#endif
