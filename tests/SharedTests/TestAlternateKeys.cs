5f3dd93a0db81e7af0d95c328c656ed20c6a76e7
Magnus Gether Sørensen <mso@delegate.dk>
2024-04-19 10:25:16 +0200

    Fixed error in extension logic when using alternate keys

--- tests/SharedTests/TestAlternateKeys.cs
+++ tests/SharedTests/TestAlternateKeys.cs
@@ -10,7 +10,7 @@ namespace DG.XrmMockupTest
     {
         public TestAlternateKeys(XrmMockupFixture fixture) : base(fixture) { }
 
-        [Fact(Skip = "Alternate key implementation should be based on column values")]
+        [Fact]
         public void TestAlternateKeysAll()
         {
             using (var context = new Xrm(orgAdminUIService))
