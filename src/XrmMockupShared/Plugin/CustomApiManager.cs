41deca514a58e7b239b588e651dc3d5639302b49
Magnus Gether Sørensen <magesoe@gmail.com>
2025-02-12 22:56:44 +0100

    Add CustomApiManager and custom API handling
    - Added a new `CustomApiManager` class to manage custom APIs in the `Core` class.
    - Updated the `Core` class to include an instance of `CustomApiManager`.
    - Modified the `Execute` method in the `Core` class to pass an additional `pluginContext` parameter to the `ExecuteRequest` method.
    - Updated the `ExecuteRequest` method to handle custom API requests by checking if the `CustomApiManager` can handle the request and executing it if so.
    - Added a new `BaseCustomApiTypes` property to the `XrmMockupSettings` class to store the base types of custom APIs.
    - Included the new `CustomApiManager.cs` file in the project files.
    - Added new custom API classes `CreateAccountApi` and `CustomAPI` to handle custom API logic.
    - Added a new test class `TestCustomApi` to test the custom API functionality.
    - Updated the `XrmMockupFixture` class to include the `BaseCustomApiTypes` setting.

---
+++ src/XrmMockupShared/Plugin/CustomApiManager.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using System.ServiceModel;
+using Microsoft.Xrm.Sdk.Metadata;
+using System.Runtime.ExceptionServices;
+using XrmMockupShared.Plugin;
+
+using MainCustomAPIConfig = System.Tuple<string, bool, int, int, int, string>;
+using ExtendedCustomAPIConfig = System.Tuple<string, string, string, bool, bool, string, string>;
+using RequestParameterConfig = System.Tuple<string, string, string, bool, bool, string, int>; // TODO: Add description maybe
+using ResponsePropertyConfig = System.Tuple<string, string, string, bool, string, int>; // TODO
+
+namespace DG.Tools.XrmMockup
+{
+    internal class CustomApiManager
+    {
+
+        private Dictionary<string, Func<MockupServiceProviderAndFactory, OrganizationResponse>> registeredApis;
+
+        public CustomApiManager(IEnumerable<Tuple<string, Type>> baseCustomApiTypes)
+        {
+            registeredApis = new Dictionary<string, Func<MockupServiceProviderAndFactory, OrganizationResponse>>();
+
+            RegisterCustomApis(baseCustomApiTypes);
+        }
+
+        private void RegisterCustomApis(IEnumerable<Tuple<string, Type>> baseCustomApiTypes)
+        {
+            foreach (var baseType in baseCustomApiTypes)
+            {
+                var prefix = baseType.Item1;
+                var baseApiType = baseType.Item2;
+                if (baseType == null) continue;
+                Assembly proxyTypeAssembly = baseApiType.Assembly;
+
+                foreach (var type in proxyTypeAssembly.GetLoadableTypes())
+                {
+                    if (type.BaseType != null && (type.BaseType == baseApiType || (type.BaseType.IsGenericType && type.BaseType.GetGenericTypeDefinition() == baseApiType)))
+                    {
+                        RegisterApi(prefix, type);
+                    }
+                }
+            }
+        }
+
+        private void RegisterApi(string prefix, Type baseType)
+        {
+            object plugin = null;
+            try
+            {
+                plugin = Activator.CreateInstance(baseType);
+            }
+            catch (Exception ex) when (ex.Source == "mscorlib" || ex.Source == "System.Private.CoreLib")
+            {
+            }
+
+            if (plugin == null)
+            {
+                return;
+            }
+
+            // Check if Matches DAXIF plugin registration
+            if (baseType.GetMethod("GetCustomAPIConfig") == null)
+                return;
+
+            var configs = baseType
+                .GetMethod("GetCustomAPIConfig")
+                .Invoke(plugin, new object[] { })
+                as Tuple<MainCustomAPIConfig, ExtendedCustomAPIConfig, IEnumerable<RequestParameterConfig>, IEnumerable<ResponsePropertyConfig>>;
+
+            Func<MockupServiceProviderAndFactory, OrganizationResponse> pluginExecute = (provider) =>
+            {
+                var resp =
+                    baseType
+                    .GetMethod("Execute")
+                    .Invoke(plugin, new object[] { provider });
+
+                return resp as OrganizationResponse;
+            };
+
+            registeredApis.Add($"{prefix}_{configs.Item1.Item1}", pluginExecute);
+        }
+
+
+        public bool HandlesRequest(string requestName)
+        {
+            return registeredApis.ContainsKey(requestName);
+        }
+
+        internal OrganizationResponse Execute(OrganizationRequest request, Core core, PluginContext pluginContext)
+        {
+            if (!registeredApis.ContainsKey(request.RequestName))
+            {
+                throw new FaultException($"Request '{request.RequestName}' is not a registered Custom API");
+            }
+
+            var thisPluginContext = pluginContext.Clone();
+            thisPluginContext.Stage = 30;
+            thisPluginContext.Mode = 0;
+
+            var serviceProvider = new MockupServiceProviderAndFactory(core, thisPluginContext, core.TracingServiceFactory);
+            registeredApis[request.RequestName](serviceProvider);
+            
+            pluginContext.OutputParameters.Clear();
+            pluginContext.OutputParameters.AddRange(thisPluginContext.OutputParameters);
+
+            return new OrganizationResponse()
+            {
+                Results = pluginContext.OutputParameters
+            };
+        }
+    }
+}
---
+++ tests/SharedPluginsAndCodeactivites/CreateAccountApi.cs
@@ -0,0 +1,33 @@
+﻿namespace DG.Some.Namespace {
+    using System;
+    using Microsoft.Xrm.Sdk;
+    using DG.XrmFramework.BusinessDomain.ServiceContext;
+    public class CreateAccountApi : CustomAPI 
+    {
+        public CreateAccountApi()
+            : base(typeof(CreateAccountApi)) 
+        {
+            RegisterCustomAPI("CreateAccount", Execute)
+                .AddRequestParameter(new CustomAPIConfig.CustomAPIRequestParameter("Name", RequestParameterType.String))
+                .AddResponseProperty(new CustomAPIConfig.CustomAPIResponseProperty("Name", RequestParameterType.String));
+        }
+
+        protected void Execute(LocalPluginContext localContext) 
+        {
+            if (localContext == null) {
+                throw new ArgumentNullException("localContext");
+            }
+
+            var service = localContext.OrganizationService;
+
+            var name = localContext.PluginExecutionContext.InputParameters["Name"] as string;
+
+            service.Create(new Entity("account")
+            {
+                ["name"] = name
+            });
+
+            localContext.PluginExecutionContext.OutputParameters["Name"] = name;
+        }
+    }
+}
