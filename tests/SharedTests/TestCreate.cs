9b6580f268a9c9288ee8584c93d1c421671f72da
Kaare Børsting <kbr@delegate.dk>
2025-04-24 17:50:02 +0200

    added related entities to typelystrongedentity, added associations in updaterequest, updated and ran tests

--- tests/SharedTests/TestCreate.cs
+++ tests/SharedTests/TestCreate.cs
@@ -278,6 +278,16 @@ namespace DG.XrmMockupTest
             var createdAccountId = orgAdminService.Create(account);
             var query = new QueryExpression(Account.EntityLogicalName) { ColumnSet = new ColumnSet(true), Criteria = new FilterExpression() };
             query.Criteria.AddCondition(new ConditionExpression(Account.GetColumnName<Account>(a => a.AccountId), ConditionOperator.Equal, createdAccountId));
+            query.LinkEntities.Add(new LinkEntity
+            {
+                Columns = new ColumnSet(true),
+                EntityAlias = Account.GetColumnName<Account>(a => a.contact_customer_accounts),
+                JoinOperator = JoinOperator.LeftOuter,
+                LinkFromEntityName = Account.EntityLogicalName,
+                LinkToEntityName = Contact.EntityLogicalName,
+                LinkFromAttributeName = Account.GetColumnName<Account>(a => a.AccountId),
+                LinkToAttributeName = Contact.GetColumnName<Contact>(c => c.contact_customer_accounts)
+            });
             var retrievedAccount = orgAdminService.RetrieveMultiple(query).Entities.FirstOrDefault();
 
             // Assert
