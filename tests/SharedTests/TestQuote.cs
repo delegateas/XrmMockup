bab58fa26d4ccedbda116b786cb1555a13f794fc
Kaare Børsting <kbr@delegate.dk>
2025-09-17 16:20:11 +0200

    chore: test cases

---
+++ tests/SharedTests/TestQuote.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using Microsoft.Crm.Sdk.Messages;
+using DG.XrmFramework.BusinessDomain.ServiceContext;
+using Xunit;
+
+namespace DG.XrmMockupTest
+{
+    public class TestQuote : UnitTestBase
+    {
+        public TestQuote(XrmMockupFixture fixture) : base(fixture) { }
+
+        [Fact]
+        public void TestWinQuote()
+        {
+            using (var context = new Xrm(orgAdminUIService))
+            {
+                var quote = new Quote();
+                quote.Id = orgAdminUIService.Create(quote);
+
+                var winReq = new WinQuoteRequest()
+                {
+                    QuoteClose = new QuoteClose()
+                    {
+                        QuoteId = quote.ToEntityReference(),
+                        Subject = "Quote won"
+                    },
+                    Status = new OptionSetValue(-1) // default to Won
+                };
+
+                orgAdminUIService.Execute(winReq);
+
+                var retrieved = orgAdminUIService.Retrieve(Quote.EntityLogicalName, quote.Id, new ColumnSet(true)) as Quote;
+
+                Assert.Equal(QuoteState.Won, retrieved.StateCode);
+                Assert.Equal(Quote_StatusCode.Won, retrieved.StatusCode);
+            }
+        }
+
+        [Fact]
+        public void TestCloseQuote()
+        {
+            using (var context = new Xrm(orgAdminUIService))
+            {
+                var quote = new Quote();
+                quote.Id = orgAdminUIService.Create(quote);
+
+                var winReq = new CloseQuoteRequest()
+                {
+                    QuoteClose = new QuoteClose()
+                    {
+                        QuoteId = quote.ToEntityReference(),
+                        Subject = "Quote closed"
+                    },
+                    Status = new OptionSetValue(-1) // default to Lost
+                };
+
+                orgAdminUIService.Execute(winReq);
+
+                var retrieved = orgAdminUIService.Retrieve(Quote.EntityLogicalName, quote.Id, new ColumnSet(true)) as Quote;
+
+                Assert.Equal(QuoteState.Closed, retrieved.StateCode);
+                Assert.Equal(Quote_StatusCode.Lost, retrieved.StatusCode);
+            }
+        }
+    }
+}
